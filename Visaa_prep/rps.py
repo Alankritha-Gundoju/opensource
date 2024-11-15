3b41248b6becb3a798838abac475a82b75bd24dc
Alankritha-Gundoju <alankrithagundoju@gmail.com>
2024-11-15 13:25:51 +0530

    Create rps.py

---
+++ Visaa_prep/rps.py
@@ -0,0 +1,12 @@
+vignesh, charan = input().split()
+if vignesh == charan:
+    print("NULL")
+elif vignesh == 'R' and charan == 'S':
+    print("Vignesh")
+elif vignesh == 'P' and charan == 'R':
+    print("Vignesh")
+elif vignesh == 'S' and charan == 'P':
+    print("Vignesh")
+else:
+    print("Charan")
+    
