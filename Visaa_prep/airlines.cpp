3889784bef8d8ebf40c8255e30bcb442baed0575
Alankritha-Gundoju <alankrithagundoju@gmail.com>
2024-11-19 11:35:35 +0530

    Create airlines.cpp

---
+++ Visaa_prep/airlines.cpp
@@ -0,0 +1,22 @@
+   #include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+       int x, n;
+    cin >> x >> n;
+    int capacity = x * 100;
+    if (capacity >= n) {
+        cout << "0";  
+        return 0;
+    }
+    int remaining_passenger = n - capacity;
+    int new_planes = (remaining_passenger + 99) / 100;  
+    cout << new_planes;
+    
+    return 0;
+}
